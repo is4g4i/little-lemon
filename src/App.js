30aea330c71345546c8584d9d1a225dcf0bbd71c
Saga <rgf-s4g4@hotmail.com>
2024-05-21 13:50:36 -0600

    Lemon init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="saga-little-lemon">
+      <header className="saga-header">
+        <nav><a href='/'>
+          Home
+        </a></nav>
       </header>
+      <body className="saga-body">
+        <h1>
+          Homepage
+        </h1>
+      </body>
     </div>
   );
 }
